0eda311e5f52f7841901784c6909d0b13d90983c
Xinyi <xie_xin_yi@hotmail.com>
2017-09-07 11:27:06 -0400

    parent listen for child event

--- src/app/voter/voter.component.ts
+++ src/app/voter/voter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-voter',
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./voter.component.css']
 })
 export class VoterComponent implements OnInit {
+  _name: string;
+  // child component exposes an EventEmitter property with which it emits
+  // events when sth happens
+  @Output() onVoted = new EventEmitter<boolean>();
 
+  voted = false;
   constructor() { }
 
   ngOnInit() {
   }
+  vote(agreed: boolean) {
+    // click function -> onVoted emit this to parent component
+    this.onVoted.emit(agreed);
+    this.voted = true;
+  }
 
+  @Input()
+  set name(name: string) {
+    this._name = name || '<No name set>';
+  }
+  get name(): string {
+    return this._name;
+  }
 }
--- src/app/votetaker/votetaker.component.ts
+++ src/app/votetaker/votetaker.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./votetaker.component.css']
 })
 export class VotetakerComponent implements OnInit {
-
+  agreed = 0;
+  disagreed = 0;
+  votes = ['Mr. Tom', 'Mr. Snape', 'Mr. Potter', ''];
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onVoted(agreed: boolean) {
+    agreed ? this.agreed++ : this.disagreed++;
   }
 
 }
